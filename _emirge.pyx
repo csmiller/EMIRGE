f6e6bab59f3fea2fc4b7284de5414e3edd360199
Chris Miller <csmiller@berkeley.edu>
2011-07-31 20:23:48 -0700

    -  Rewrote substantial part of the code with aim of making likelihood calculation faster.  Moved some of the likelihood calculation to Cython.  The likelihood function now typically takes < 1/2 the time it used to, and iterations as a whole typically see a speed up of roughly 150 to 200  percent. -  Added setup.py script to aid in building Cython C module (_emirge.so), and to install emirge. -  Modified README.txt to indicate that Cython is now a dependency (it already was implicitly for pysam) and to give quick installation instructions.

---
+++ _emirge.pyx
@@ -0,0 +1,84 @@
+"""
+_emirge.pyx contains helper cython functions for emirge.py
+
+EMIRGE: Expectation-Maximization Iterative Reconstruction of Genes from the Environment
+Copyright (C) 2010 Christopher S. Miller  (csmiller@berkeley.edu)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+https://github.com/csmiller/EMIRGE
+"""
+
+from libc.stdlib cimport malloc, free
+from libc.math cimport M_E, log, pow as c_pow
+import numpy as np
+cimport numpy as np
+import sys
+
+# for lookup table of qual values
+cdef extern from *:
+    ctypedef double* static_const_double_ptr "static const double*"
+cdef extern from "_emirge_C.h":
+    static_const_double_ptr qual2one_minus_p   # lookup tables to avoid too many double calcs.
+    static_const_double_ptr qual2p_div_3
+
+cpdef int base_alpha2int(char base_ascii):
+    """
+    base2i = {"A":0,"T":1,"C":2,"G":3}
+    """
+    if base_ascii == <int>('A'):
+        return 0
+    elif base_ascii == <int>('T'):
+        return 1
+    elif base_ascii == <int>('C'):
+        return 2
+    elif base_ascii == <int>('G'):
+        return 3
+    else:
+        return 4
+        
+cpdef double calc_likelihood_cell(unsigned int seq_i, unsigned int read_i, unsigned int pair_i,
+                                  unsigned int rlen, unsigned int qlen, unsigned int pos,
+                                  np.ndarray[np.uint8_t, ndim=1] numeric_bases,
+                                  np.ndarray[np.uint8_t, ndim=1] qualints,
+                                  np.ndarray[np.float_t, ndim=2] probN):  # an individual probN numpy matrix
+    """
+    numeric_bases is an array of unsigned char (numpy.ubyte)  already converted to 0-4-based index
+    
+    return single entry for lik_data, keeping main looping in python still
+    RETURNS: double to place in lik_data
+    """
+    cdef double p = 0.0
+    cdef double s
+
+    cdef int i
+    cdef int j
+
+    for i in range(numeric_bases.shape[0]):
+    # for i from 0 <= i < numeric_bases.shape[0]:  # old syntax, range syntax above fast if i is cdef-ed.
+        s = 0.0
+        for j in range(4):
+            if numeric_bases[i] == j:   # this is called base, set 1-P
+                # s += (1. - error_p_i) * probN[pos + i, j]
+                s += ( qual2one_minus_p[qualints[i]] * probN[pos + i, j] )    # lookup table
+            else:                       # this is not the called base, so set to P/3
+                # s += (error_p_i / 3 ) * probN[pos + i, j]
+                s += ( qual2p_div_3[qualints[i]] * probN[pos + i, j] )          # lookup table
+        p += log(s)
+        
+    # do at least this product in log space (0.94005726833168002 vs. 0.94005726833167991)
+    # likelihood[seq_i, read_i] = e**(np_log(prob_b_i).sum())
+    return c_pow(M_E, p)
+    
+    
