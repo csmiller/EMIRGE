43a4c3e062ccbc214447b4265e95a3e7ae641007
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2015-11-23 18:39:35 -0700

    fix flake8 reported style errors

--- utils/fix_nonstandard_chars.py
+++ utils/fix_nonstandard_chars.py
@@ -13,7 +13,8 @@ From the bowtie manual
 ambiguous characters in the reference; alignments involving ambiguous
 characters in the read are legal, subject to the alignment
 policy. Ambiguous characters in the read mismatch all other
-characters. Alignments that"fall off" the reference sequence are not considered valid."
+characters. Alignments that "fall off" the reference sequence are not
+considered valid."
 
 Where IUPAC codes specifying only some of the possible bases are
 given, one from the appropriate subset of bases will replace the
@@ -29,25 +30,25 @@ from Bio import SeqIO, Seq
 from random import choice
 
 
-def main(inf = None, outf = None):
+def main(inf=None, outf=None):
     replace_dict = {"-": "",
                     ".": "",
-                    "A": "A",	
+                    "A": "A",
                     "C": "C",
                     "G": "G",
                     "T": "T",
                     "U": "T",
-                    "R": "AG", #	Purine (A or G)
-                    "Y": "CT", #	Pyrimidine (C, T, or U)
-                    "M": "CA", #	C or A
-                    "K": "TG", #	T, U, or G
-                    "W": "TA", #	T, U, or A
-                    "S": "CG", #	C or G
-                    "B": "CTG",#	C, T, U, or G (not A)
-                    "D": "ATG",#	A, T, U, or G (not C)
-                    "H": "ATC",#	A, T, U, or C (not G)
-                    "V": "ACG",#	A, C, or G (not T, not U)
-                    "N": "ACTG" }#	Any base (A, C, G, T, or U)
+                    "R": "AG",        # Purine (A or G)
+                    "Y": "CT",        # Pyrimidine (C, T, or U)
+                    "M": "CA",        # C or A
+                    "K": "TG",        # T, U, or G
+                    "W": "TA",        # T, U, or A
+                    "S": "CG",        # C or G
+                    "B": "CTG",       # C, T, U, or G (not A)
+                    "D": "ATG",       # A, T, U, or G (not C)
+                    "H": "ATC",       # A, T, U, or C (not G)
+                    "V": "ACG",       # A, C, or G (not T, not U)
+                    "N": "ACTG"}      # Any base (A, C, G, T, or U)
     if inf is None:
         inf = sys.stdin
     if outf is None:
@@ -61,10 +62,10 @@ def main(inf = None, outf = None):
     get = replace_dict.get
 
     for record in SeqIO.parse(inf, 'fasta'):
-        newseqstring = joinstring([_choice(get(x, "ACTG")) for x in _str(record.seq.upper())])
-        record.seq =_Seq(newseqstring)
+        newseqstring = joinstring([_choice(get(x, "ACTG"))
+                                  for x in _str(record.seq.upper())])
+        record.seq = _Seq(newseqstring)
         outf_write(record.format("fasta"))
 
 if __name__ == "__main__":
     main()
-
