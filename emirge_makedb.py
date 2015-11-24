9eba4e9d1e19966454bb47a216481b005c45bf0b
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2015-11-24 13:28:01 -0700

    fix flake8 reported style issues, add docstrings

--- emirge_makedb.py
+++ emirge_makedb.py
@@ -3,52 +3,44 @@
 import urllib2
 import urllib
 import re
-import sys, os
+import sys
+import os
 import hashlib
-from optparse import OptionParser, OptionGroup
+from optparse import OptionParser
 import random
 import subprocess
 
-USAGE = \
-"""usage: %prog [OPTIONS]
+USAGE = """usage: %prog [OPTIONS]
 
-%prog creates a reference database and the necessay indices for use by EMIRGE
-from an rRNA reference database. Without extra parameters, %prog will 1) download
-the most recent SILVA SSU database, 2) filter it by sequence length, 3) cluster at
-97% sequence identity, 4) replace ambiguous bases with random characters and 5) 
-create a bowtie index.
+%prog creates a reference database and the necessay indices for use by
+EMIRGE from an rRNA reference database. Without extra parameters, %prog
+will 1) download the most recent SILVA SSU database, 2) filter it by sequence
+length, 3) cluster at 97% sequence identity, 4) replace ambiguous bases
+with random characters and 5) create a bowtie index.
 """
 
 SILVA_BASEURL = "https://ftp.arb-silva.de"
 SILVA_SSUFILE = "SILVA_{0}_SSURef_Nr99_tax_silva_trunc.fasta.gz"
 SILVA_LSUFILE = "SILVA_{0}_LSURef_tax_silva_trunc.fasta.gz"
 
-iupac_map = {
-    "": " ",
-    "R": "AG", #	Purine (A or G)
-    "Y": "CT", #	Pyrimidine (C, T, or U)
-    "M": "CA", #	C or A
-    "K": "TG", #	T, U, or G
-    "W": "TA", #	T, U, or A
-    "S": "CG", #	C or G
-    "B": "CTG",#	C, T, U, or G (not A)
-    "D": "ATG",#	A, T, U, or G (not C)
-    "H": "ATC",#	A, T, U, or C (not G)
-    "V": "ACG",#	A, C, or G (not T, not U)
-    "N": "ACTG" }#	Any base (A, C, G, T, or U)
 
 class DownloadException(Exception):
+    """Used to pass the URL downwards for urllib(2) exceptions"""
     pass
 
+
 def fetch_url(url):
+    """Fetches URL contents into variable"""
     try:
         return urllib2.urlopen(url).read()
     except urllib2.URLError as e:
         raise DownloadException(
-            "Unable to fetch \"{0}\":\n \"{1}\"".format(license_url, e.reason)
+            "Unable to fetch \"{0}\":\n \"{1}\"".format(url, e.reason)
             )
 
+
 def print_progress(block, blocksize, total):
+    """Callback function to print progress during download"""
     blocks = int((total-1)/blocksize) + 1
     linewidth = min(77, blocks)
     if block == 0:
@@ -59,45 +51,61 @@ def print_progress(block, blocksize, total):
         if (block % (blocks/linewidth) == 0):
             sys.stdout.write(".")
             sys.stdout.flush()
-    
+
+
 def download_url(url, folder):
+    """Downloads URL as file into folder. Returns filename."""
     print "Downloading \"{0}\" to \"{1}\"".format(url, folder)
-        
+
     filename = os.path.join(folder, url.split("/")[-1])
     if os.path.isfile(filename):
         existing_file_size = os.path.getsize(filename)
-        remote_file_size = urllib.urlopen(url).info().getheaders("Content-Length")[0]
+        urlinfo = urllib.urlopen(url).info()
+        remote_file_size = urlinfo.getheaders("Content-Length")[0]
         if int(existing_file_size) == int(remote_file_size):
-            print "Found existing file matching remote size. Skipping download."
+            print (
+                "Found existing file matching remote size. Skipping download."
+            )
             return filename
         else:
-            msg = "Local file with size {0} does not match remote file size {1}."
-            print msg.format(existing_file_size, remote_file_size)
-            
+            print (
+                "Local file with size {0} does not match remote file size {1}."
+                .format(existing_file_size, remote_file_size)
+            )
+
     (filename, header) = urllib.urlretrieve(url, filename, print_progress)
-    
+
     return filename
 
+
 def compute_file_md5(filename):
+    """Computes the MD5 checksum of a file"""
     hash = hashlib.md5()
     with open(filename, "rb") as file:
         for chunk in iter(lambda: file.read(4096), b""):
             hash.update(chunk)
     return hash.hexdigest()
-    
+
+
 def silva_find_most_recent_version():
+    """Determines most recent SILVA release found at SILVA_BASEURL"""
     url = "/".join([SILVA_BASEURL, "current", "Exports"])
-    files = re.findall("<a href=\"SILVA_([[0-9_]*)_",fetch_url(url))
+    files = re.findall("<a href=\"SILVA_([[0-9_]*)_", fetch_url(url))
     return files[0]
 
+
 def silva_confirm_license(release):
-    license_url = "/".join([SILVA_BASEURL, "release_" + release.replace(".","_"),
+    """Prompts user to confirm SILVA license found for `release`
+    Raises DownloadException if confirmation fails.
+    """
+    license_url = "/".join([SILVA_BASEURL,
+                            "release_" + release.replace(".", "_"),
                             "Exports", "LICENSE.txt"])
     license_txt = fetch_url(license_url)
-                           
+
     print("""
-The SILVA database is published under a custom license. To proceed, you need to read this
-license and agree with its terms:
+The SILVA database is published under a custom license. To proceed, you need
+to read this license and agree with its terms:
     """)
     print ("> "+license_txt.replace("\n", "\n> ").rstrip("\n> "))
     print ""
@@ -105,18 +113,22 @@ license and agree with its terms:
     answer = raw_input("Do you agree to these terms? [yes|NO]")
     if (answer != "yes"):
         raise DownloadException("Unable to continue")
-        
+
 
 def silva_download_fasta(release, gene, tmpdir):
+    """Downloads SILVA database for `release` and `gene` into tmpdir.
+    Checks file integrity by comparing MD5 checksums.
+    """
     filename = SILVA_SSUFILE if gene.upper() == "SSU" else SILVA_LSUFILE
     filename = filename.format(release)
-    database_url = "/".join([SILVA_BASEURL, "release_" + release.replace(".","_"),
+    database_url = "/".join([SILVA_BASEURL,
+                             "release_" + release.replace(".", "_"),
                              "Exports", filename])
     silva_fasta = download_url(database_url, tmpdir)
-    silva_md5   = download_url(database_url + ".md5", tmpdir)
+    silva_md5 = download_url(database_url + ".md5", tmpdir)
     print "Verifying file...",
     with open(silva_md5, "rb") as file:
-          silva_md5 = file.read().split(" ")[0]
+        silva_md5 = file.read().split(" ")[0]
     local_md5 = compute_file_md5(silva_fasta)
     if (local_md5 == silva_md5):
         print "OK"
@@ -125,18 +137,24 @@ def silva_download_fasta(release, gene, tmpdir):
         print "FAILED"
         raise DownloadException("Corrupted File?!")
 
-def cluster_fasta(binary, filein, minlen, maxlen, clusterid):
+
+def cluster_fasta(vsearch_bin, filein, minlen, maxlen, clusterid):
+    """Runs vsearch cluster_fast on `filein`, considering only sequences
+    with `minlen` <= sequence length <= `maxlen`
+    """
     filein_split = filein.split(".")
-    fileout = ".".join(filein_split[0:-2] +
-                       ["ge{0}bp".format(minlen), "le{0}bp".format(maxlen), str(clusterid)] +
-                       filein_split[-2:-1]
-                      )
+    fileout = ".".join(
+        filein_split[0:-2] +
+        ["ge{0}bp".format(minlen), "le{0}bp".format(maxlen), str(clusterid)] +
+        filein_split[-2:-1]
+    )
 
     if os.path.isfile(fileout):
-        print "Found existing file \"{0}\". Skipping clustering stage.".format(fileout)
+        print ("Found existing file \"{0}\". Skipping clustering stage."
+               .format(fileout))
         return fileout
-    
-    cmd = [binary,
+
+    cmd = [vsearch_bin,
            "--minseqlength", str(minlen),
            "--maxseqlength", str(maxlen),
            "--fasta_width", "0",
@@ -148,34 +166,65 @@ def cluster_fasta(binary, filein, minlen, maxlen, clusterid):
     print (" ".join(["Running: "] + cmd))
     subprocess.call(cmd)
     print "Done"
-    
+
     return fileout
 
+
 def pairs(lst):
-    it   = iter(lst)
+    """Creates pairwise iterator on `lst`.
+    I.e. if lst returns 1..5, pairs(lst) will return (1,2), (3,4), (5,"")
+    """
+    it = iter(lst)
     for item in it:
         try:
             yield item, it.next()
         except StopIteration:
             yield item, ""
 
+
+iupac_map = {
+    "": " ",
+    "R": "AG",    # Purine (A or G)
+    "Y": "CT",    # Pyrimidine (C, T, or U)
+    "M": "CA",    # C or A
+    "K": "TG",    # T, U, or G
+    "W": "TA",    # T, U, or A
+    "S": "CG",    # C or G
+    "B": "CTG",   # C, T, U, or G (not A)
+    "D": "ATG",   # A, T, U, or G (not C)
+    "H": "ATC",   # A, T, U, or C (not G)
+    "V": "ACG",   # A, C, or G (not T, not U)
+    "N": "ACTG"   # Any base (A, C, G, T, or U)
+}
+
+
 def randomize_ambiguous(seq):
-    #return "".join([choice(iupac_map.get(x, "ACTG")) for x in seq.upper()])
-    seq = seq.upper().replace("U","T")
-    list = [ ok + random.choice(iupac_map.get(fix, "ACGT"))
-             for ok, fix in pairs(re.split("([^ACGT])", seq)) ]
+    """Replaces IUPAC ambiguous characters in seq with random characters,
+    respecting options, i.e. "R" replaced with A or G.
+    """
+    # slower (but clearer) implementation:
+    # return "".join([choice(iupac_map.get(x, "ACTG")) for x in seq.upper()])
+    seq = seq.upper().replace("U", "T")
+    list = [ok + random.choice(iupac_map.get(fix, "ACGT"))
+            for ok, fix in pairs(re.split("([^ACGT])", seq))]
     return "".join(list).rstrip(" ")
 
-def randomize_ambiguous_fasta(filein, folder = None):
+
+def randomize_ambiguous_fasta(filein, folder=None):
+    """Replaces IUPAC ambiguous character in `filein` with random choice
+    from allowed characters.
+    Returns output filename.
+    """
     filein_split = filein.split(".")
     fileout = ".".join(filein_split[0:-1] + ["fixed"] + filein_split[-1:])
-    if folder != None:
+    if folder is not None:
         fileout = os.path.join(folder, os.path.basename(fileout))
 
     if os.path.exists(fileout):
-        print "Found existing file \"{0}\". Skipping randomization stage.".format(fileout)
+        print ("Found existing file \"{0}\". Skipping randomization stage."
+               .format(fileout))
         return fileout
-    
+
     processed = 0
     total = os.path.getsize(filein)
     dots = 0
@@ -193,44 +242,65 @@ def randomize_ambiguous_fasta(filein, folder = None):
             dotstoprint = int(linewidth * processed / total) - dots
             sys.stdout.write("."*dotstoprint)
             sys.stdout.flush()
-            dots+=dotstoprint
-            
+            dots += dotstoprint
+
     return fileout
 
-def build_bowtie_index(binary, filein):
+
+def build_bowtie_index(bowtie_bin, filein):
+    """Calls bowtie-build on `filein` to compute bowtie index"""
     fileout = ".".join(filein.split(".")[:-1])
-    cmd = [binary, "-o", "0", filein, fileout]
+    cmd = [bowtie_bin, "-o", "0", filein, fileout]
     print "Running: " + " ".join(cmd)
     subprocess.call(cmd)
 
-def main(argv = sys.argv[1:]):
+
+def main(argv=sys.argv[1:]):
     parser = OptionParser(USAGE)
-    parser.add_option("-g", "--gene", dest="gene", metavar="[SSU|LSU]", type="string",
-                      default="SSU",
-                      help="build database from this gene")
-    parser.add_option("-t", "--tmpdir", dest="tmpdir", metavar="DIR", type="string",
-                      default="/tmp",
-                      help="work directory for temporary files")
-    parser.add_option("-r", "--release", dest="release", metavar="N", type="string",
-                      default="current",
-                      help="SILVA release number")
-    parser.add_option("-m", "--min-len", dest="min_len", metavar="LEN", type="int",
-                      default=1200,
-                      help="minimum reference sequence length")
-    parser.add_option("-M", "--max-len", dest="max_len", metavar="LEN", type="int",
-                      default=2000,
-                      help="maximum reference sequence length")
-    parser.add_option("-i", "--id", dest="clusterid", metavar="FLOAT", type="float",
-                      default=0.97,
-                      help="Cluster at this identity level")
-    parser.add_option("-k", "--keep", dest="keep", action="store_true",
-                      help="keep intermediary files")
-    parser.add_option("-V", "--vsearch", dest="vsearch", metavar="FILE", type="string",
-                      default="vsearch",
-                      help="path to vsearch binary")
-    parser.add_option("-B", "--bowtie-build", dest="bowtie", metavar="FILE", type="string",
-                      default="bowtie-build",
-                      help="path to bowtie-build binary")
+    parser.add_option(
+        "-g", "--gene", dest="gene", metavar="[SSU|LSU]", type="string",
+        default="SSU",
+        help="build database from this gene"
+    )
+    parser.add_option(
+        "-t", "--tmpdir", dest="tmpdir", metavar="DIR", type="string",
+        default="/tmp",
+        help="work directory for temporary files"
+    )
+    parser.add_option(
+        "-r", "--release", dest="release", metavar="N", type="string",
+        default="current",
+        help="SILVA release number"
+    )
+    parser.add_option(
+        "-m", "--min-len", dest="min_len", metavar="LEN", type="int",
+        default=1200,
+        help="minimum reference sequence length"
+    )
+    parser.add_option(
+        "-M", "--max-len", dest="max_len", metavar="LEN", type="int",
+        default=2000,
+        help="maximum reference sequence length"
+    )
+    parser.add_option(
+        "-i", "--id", dest="clusterid", metavar="FLOAT", type="float",
+        default=0.97,
+        help="Cluster at this identity level"
+    )
+    parser.add_option(
+        "-k", "--keep", dest="keep", action="store_true",
+        help="keep intermediary files"
+    )
+    parser.add_option(
+        "-V", "--vsearch", dest="vsearch", metavar="FILE", type="string",
+        default="vsearch",
+        help="path to vsearch binary"
+    )
+    parser.add_option(
+        "-B", "--bowtie-build", dest="bowtie", metavar="FILE", type="string",
+        default="bowtie-build",
+        help="path to bowtie-build binary"
+    )
 
     (options, args) = parser.parse_args(argv)
 
@@ -239,10 +309,13 @@ def main(argv = sys.argv[1:]):
 
     try:
         silva_confirm_license(options.release)
-        silva_fasta      = silva_download_fasta(options.release, options.gene, options.tmpdir)
-        clustered_fasta  = cluster_fasta(options.vsearch, silva_fasta,
-                                         options.min_len, options.max_len, options.clusterid)
-        randomized_fasta = randomize_ambiguous_fasta(clustered_fasta, folder="")
+        silva_fasta = silva_download_fasta(options.release, options.gene,
+                                           options.tmpdir)
+        clustered_fasta = cluster_fasta(options.vsearch, silva_fasta,
+                                        options.min_len, options.max_len,
+                                        options.clusterid)
+        randomized_fasta = randomize_ambiguous_fasta(clustered_fasta,
+                                                     folder="")
         build_bowtie_index(options.bowtie, randomized_fasta)
         if (not options.keep):
             os.unlink(silva_fasta)
@@ -254,7 +327,5 @@ def main(argv = sys.argv[1:]):
         exit(1)
 
 
-
 if __name__ == '__main__':
     main()
-    
