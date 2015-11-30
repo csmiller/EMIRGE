b1daadbcc0cfdeb6fc193d5d975c9c362c2a6428
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2015-11-29 23:27:51 -0700

    flake8 fix

--- Emirge/io.py
+++ Emirge/io.py
@@ -2,6 +2,7 @@
 
 import re
 
+
 class Record:
     """
     stripped down FASTA record class with same members as
@@ -9,32 +10,38 @@ class Record:
         title    -- with > removed
         sequence -- as string
     """
-    _colwidth = 60 # colwidth specifies the number of residues to put on each line when generating FASTA format.
-    def __init__(self, title = "", sequence = ""):
+    _colwidth = 60  # number of residues per FASTA line
+
+    def __init__(self, title="", sequence=""):
         """
         Create a new Record.
 
         """
         self.title = title
         self.sequence = sequence
-    def __str__(self):
-        return ">%s\n%s\n"%(self.title, "\n".join(re.findall(".{1,%s}"%self._colwidth, self.sequence)))
 
-def FastIterator(filehandle, dummyParser = None, record = None):
+    def __str__(self):
+        return ">%s\n%s\n" % (
+            self.title,
+            "\n".join(re.findall(".{1,%s}" % self._colwidth, self.sequence))
+        )
+
+
+def FastIterator(filehandle, dummyParser=None, record=None):
     """
-    a generator to return records one at a time.  Maybe 160% faster on test case.
+    Generator returning Records from FASTA. Maybe 160% faster on test case.
     MAY RAISE MemoryError ON LARGE FASTA FILES
     IN:  file object
-         dummyParser is a placeholder for RecordParser from Biopython.  Not used.
-         a record to use for yielding.  Otherwise create an empty one with standard init
+         dummyParser is a placeholder for RecordParser from Biopython.  Unused.
+         (optional) record to use as template
 
-    NOTE: this fasta iterator is fast, but it breaks easily with nonstandard input, for example
-    if there are "\r" in endlines.
+    NOTE: this iterator is fast, but breaks easily with nonstandard input,
+    e.g. if there are "\r" in endlines.
     """
     if record is None:
         record = Record()
 
     for recordstring in re.split('\n>', filehandle.read()[1:]):
-        record.title, record.sequence = recordstring.split('\n',1)
-        record.sequence = record.sequence.replace('\n','').replace(' ','')
+        record.title, record.sequence = recordstring.split('\n', 1)
+        record.sequence = record.sequence.replace('\n', '').replace(' ', '')
         yield record
