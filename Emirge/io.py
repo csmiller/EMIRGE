9444c719e68216430005b0b470edd46693243d2b
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2016-01-04 21:12:57 -0700

    add code to handle external commands

--- Emirge/io.py
+++ Emirge/io.py
@@ -1,13 +1,25 @@
 """Implements IO functions"""
 
 import re
-from tempfile import NamedTemporaryFile
-from subprocess import CalledProcessError, check_call, Popen, PIPE
+import os
+import weakref
+import stat
+from tempfile import NamedTemporaryFile, mkdtemp
+import subprocess
+from subprocess import CalledProcessError, check_call, PIPE
 
-from Emirge import log
+from Emirge.log import ERROR, DEBUG, INFO, timed
 
 
-class Record:
+def ispipe(path):
+    """Checks if file at @path is a pipe"""
+    try:
+        return stat.S_ISFIFO(os.stat(path).st_mode)
+    except OSError:
+        return False
+
+
+class Record(object):
     """
     Stripped down FASTA record class with same members as
     Biopython FASTA Record Class:
@@ -54,8 +66,8 @@ def FastIterator(filehandle, dummyParser=None, record=None):
         yield record
 
 
-@log.timed("Rewriting reads with indices in headers")
-def ReindexReads(reads_filepath):
+@timed("Rewriting reads with indices in headers")
+def reindex_reads(reads_filepath):
     """
     Replaces sequence headers ("@...") with the sequence number.
     Although this requires an inefficient rewrite of the fastq file,
@@ -70,33 +82,385 @@ def ReindexReads(reads_filepath):
     tmp_n_reads_file_path = NamedTemporaryFile()
     new_reads_filepath = NamedTemporaryFile(suffix="reindexed_reads.fq")
 
-    try:
-        if reads_filepath.endswith('.gz'):
-            src = Popen(["gunzip", "-c", reads_filepath],
-                        stdout=PIPE).stdout
+    with decompressed(reads_filepath).reader() as src:
+        try:
+            check_call(['awk',
+                        '{ if ((NR-1) %% 4 == 0) { print "@"(NR-1)/4;nr=nr+1 }'
+                        'else { print } }'
+                        'END { print nr > "%s" }'
+                        % tmp_n_reads_file_path.name],
+                       stdin=src, stdout=new_reads_filepath)
+        except CalledProcessError:
+            ERROR("awk rewrite of reads failed! Is awk installed?")
+            raise
+
+    new_reads_filepath.file.seek(0)
+    n_reads = int(tmp_n_reads_file_path.readline().strip())
+
+    return new_reads_filepath, n_reads
+
+
+class TempDir(object):
+    """Creates a temporary directory
+    The directory is cleaned automatically at object deletion."""
+    def __init__(self, suffix="", prefix="tmp", dir=None):
+        self.__suffix = suffix
+        self.__prefix = prefix
+        self.__dir = dir
+        self.__name = None
+
+    def __del__(self):
+        if self.__name:
+            DEBUG("Unlinking temporary directory \"%s\"" % self.__name)
+            try:
+                os.rmdir(self.__name)
+            except OSError, e:
+                ERROR("Unlinking failed: %s" % e.strerror)
+
+    @property
+    def name(self):
+        if not self.__name:
+            self.__name = mkdtemp(self.__suffix, self.__prefix, self.__dir)
+            DEBUG("Created temporary directory \"%s\"" % self.__name)
+
+        return self.__name
+
+
+class NamedPipe(object):
+    """Creates a named pipe
+    Named pipes are created in a shared temporary directory, pipes and
+    directory are automatically removed once the NamedPipe object is destroyed
+    """
+    numPipes = 0  # number of created named pipes
+    pipes_dir = None  # holds WeakRef to TempDir object
+
+    def __init__(self, suffix="", prefix="pipe", dir=None):
+        self.__suffix = suffix
+        self.__prefix = prefix
+        self.__dir = dir
+        self.__name = None
+
+        self.__pipeNum = NamedPipe.numPipes
+        NamedPipe.numPipes += 1
+
+        if self.__dir is None:
+            pipes_dir = NamedPipe.pipes_dir
+            if pipes_dir is None or pipes_dir() is None:
+                # no pipe dir created, or pipe dir deleted (weakref evaluates
+                #  to None)
+                # => create new TempDir and store weak reference in static
+                #  pipes_dir
+                self.__dir = TempDir("pipes")
+                NamedPipe.pipes_dir = weakref.ref(self.__dir)
+            else:
+                # reference shared pipe directory
+                self.__dir = pipes_dir()
+
+    def __del__(self):
+        if self.__name:
+            DEBUG("Unlinking named pipe \"%s\"" % self.__name)
+            os.unlink(self.__name)
+
+    @property
+    def name(self):
+        if not self.__name:
+            filename = "_".join([self.__prefix, str(self.__pipeNum),
+                                 self.__suffix])
+            self.__name = os.path.join(self.__dir.name, filename)
+            os.mkfifo(self.__name)
+            DEBUG("Created named pipe \"%s\"" % self.__name)
+        return self.__name
+
+
+class FileName(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class InputFileName(FileName):
+    """Class to contain the path to an input file. Checks that file exists
+    and is readable in constructor."""
+
+    def __init__(self, name, check=True):
+        super(InputFileName, self).__init__(name)
+        self.mode = "rb"
+
+        if not check:
+            return
+
+        # do some checking to tell user about potential problems at startup
+        # (these errors still have to be checked later, as the accessibility
+        # of the file may have changed until then)
+        msg = 'Input file "{}" '.format(name)
+        if not os.path.exists(name):
+            raise Exception(msg + "does not exist")
+        if os.path.isdir(name):
+            raise Exception(msg + "is a directory (file expected)")
+        if not os.path.isfile(name) and not ispipe(name):
+            raise Exception(msg + " is neither file nor pipe")
+        if not os.access(name, os.R_OK):
+            raise Exception(msg + "cannot be read")
+
+
+class OutputFileName(FileName):
+    def __init__(self, name, check=True, overwrite=True):
+        super(OutputFileName, self).__init__(name)
+        self.mode = "wb+"
+
+        if not check:
+            return
+
+        msg = 'Output file "{}" '.format(name)
+        if os.path.isdir(name):
+            raise Exception(msg + "is a directory (file expected)")
+        if not os.path.exists(os.path.dirname(name)):
+            raise Exception(msg + "cannot be created: directory does not exist")
+        if not os.path.isfile(name) and not ispipe(name):
+            if not os.access(os.path.dirname(name), os.W_OK):
+                raise Exception(msg + "cannot be created: " +
+                                      "directory is not writable")
+        if os.path.isfile(name):
+            if not overwrite:
+                raise Exception(msg + "exists; cowardly refusing to overwrite")
+            if not os.access(name, os.W_OK):
+                raise Exception(msg + "is write protected")
+
+
+class FileLike(object):
+    def __init__(self):
+        self.mode = None
+        self.infile = None
+        self.outfile = None
+
+    def reader(self):
+        self.mode = "rb"
+        return self
+
+    def writer(self):
+        self.mode = "wb+"
+        return self
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.outfile.next()
+
+    def write(self, string):
+        return self.infile.write(string)
+
+    def writelines(self, lines):
+        return self.infile.writelines(lines)
+
+    def fileno(self):
+        if self.infile:
+            if self.outfile:
+                ERROR("both in and out?")
+            # FIXME: hacky workaround for pysam
+            # pysam closes the fd, leading to an error once the python file
+            # object is closed. to fix this, we create a duplicate fd
+            return os.dup(self.infile.fileno())
+        if self.outfile:
+            return os.dup(self.outfile.fileno())
+        raise Exception("neither in nor out?")
+
+    def close(self):
+        if self.infile:
+            self.infile.close()
+        if self.outfile:
+            self.outfile.close()
+
+
+class File(FileLike):
+    def __init__(self, filename):
+        super(File, self).__init__()
+        self.__filename = filename
+
+    @property
+    def name(self):
+        return self.__filename
+
+    def __enter__(self):
+        if self.mode == "rb":
+            self.outfile = open(self.__filename, self.mode)
+        if self.mode == "wb+":
+            self.infile = open(self.__filename, self.mode)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
+class Popen(subprocess.Popen):
+    """Like the original, but logs and has close_fds=True"""
+    def __init__(self, args, *otherargs, **kwargs):
+        cmdline = " ".join([str(x) for x in args])
+        INFO("Executing '{}'".format(cmdline))
+        kwargs["close_fds"] = True
+        super(Popen, self).__init__(args, *otherargs, **kwargs)
+
+
+class Pipe(FileLike):
+    cmd = None
+    name = "-"  # needed to make pysam accept this as a pipe
+
+    def __init__(self, infile=PIPE, outfile=PIPE):
+        super(Pipe, self).__init__()
+        if isinstance(infile, FileLike):
+            self.__input = infile.reader()
         else:
-            src = open(reads_filepath)
+            self.__input = infile
 
-        check_call(['awk',
-                    '{ if ((NR-1) %% 4 == 0) { print "@"(NR-1)/4 }'
-                    'else { print } }'
-                    'END { print (NR)/4 > "%s" }'
-                    % (tmp_n_reads_file_path.name)],
-                   stdin=src, stdout=new_reads_filepath)
+        if isinstance(outfile, FileLike):
+            self.__output = outfile.writer()
+        else:
+            self.__output = outfile
 
-        n_reads = int(tmp_n_reads_file_path.readline().strip())
-    except CalledProcessError:
-        log.error("awk rewrite of reads failed! Is awk installed?")
-        raise
+        self.__proc = None
+        self.closed = True
+        self.__input_entered = None
+        self.__output_entered = None
+        self.__tmppipe = None
 
-    new_reads_filepath.seek(0)
-    return (new_reads_filepath, n_reads)
+    def __enter__(self):
+        to_close = []
+        # enter input
+        if isinstance(self.__input, FileLike):
+            self.__input_entered = self.__input.__enter__().fileno()
+        elif hasattr(self.__input, "name"):
+            self.__input_entered = open(self.__input.name, "rb+")
+            to_close.append(self.__input_entered)
+        else:
+            self.__input_entered = self.__input
 
-@log.timed("Counting reads in input files")
-def FastqCountReads(filename):
-    cmd = "cat %s | wc -l" % (filename)
-    if filename.endswith('.gz'):
-        cmd = "z" + cmd
-    p = Popen(cmd, shell=True, stdout=PIPE)
-    stdoutdata, stderrdata = p.communicate()
-    return int(stdoutdata.strip()) / 4
+        # enter ouput
+        if isinstance(self.__output, FileLike):
+            self.__output_entered = self.__output.__enter__().fileno()
+        elif isinstance(self.__output, NamedPipe):
+            self.__output_entered = open(self.__output.name, "wb+")
+            to_close.append(self.__output_entered)
+        else:
+            self.__output_entered = self.__output
+
+        # replace Pipe objects with named pipes
+        args = [x.enter_as_named_pipe() if isinstance(x, Pipe) else x
+                for x in self.cmd]
+
+        self.__proc = Popen(args, close_fds=True,
+                            stdin=self.__input_entered,
+                            stdout=self.__output_entered)
+
+        for f in to_close:
+            f.close()
+
+        if self.__input == PIPE:
+            self.infile = self.__proc.stdin
+        if self.__output == PIPE:
+            self.outfile = self.__proc.stdout
+        self.closed = False
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if isinstance(self.__output, FileLike):
+            self.__output.__exit__(exc_type, exc_val, exc_tb)
+        if isinstance(self.__input, FileLike):
+            self.__input.__exit__(exc_type, exc_val, exc_tb)
+        for x in self.cmd:
+            if isinstance(x, Pipe):
+                x.__exit__(exc_type, exc_val, exc_tb)
+        self.closed = True
+        self.__tmppipe = None
+        self.__proc.wait()
+        del self.__proc
+        self.__proc = None
+        del self.outfile
+        self.outfile = None
+        self.infile = None
+
+    def enter_as_named_pipe(self):
+        if self.__input == PIPE:
+            self.__input = NamedPipe(suffix=type(self).__name__)
+            self.__tmppipe = self.__input
+            self.__enter__()
+            self.__input == PIPE
+        elif self.__output == PIPE:
+            self.__output = NamedPipe(suffix=type(self).__name__)
+            self.__tmppipe = self.__output
+            self.__enter__()
+            self.__output = PIPE
+        else:
+            raise Exception("named pipe with infile={} and outfile={}"
+                            .format(self.infile, self.outfile))
+
+        return self.__tmppipe.name
+
+
+def make_pipe(name, args):
+    def __init__(self, filename):
+        Pipe.__init__(self, filename)
+    return type(name, (Pipe,), {"__init__": __init__,
+                                "cmd": args})
+
+
+Gunzip = make_pipe("Gunzip", ["gzip", "-dc"])
+Unxz = make_pipe("Unxz", ["xz", "-dc"])
+Unlz4 = make_pipe("Unlz4", ["lz4", "-dc"])
+Bunzip2 = make_pipe("Bunzip2", ["bzip2", "-dc"])
+
+Gzip = make_pipe("Gzip", ["gzip", "-c"])
+
+
+def decompressed(filething):
+    if isinstance(filething, str):
+        name = filething
+        filething = File(filething)
+    else:
+        name = filething.name
+
+    if name.endswith(".gz"):
+        return Gunzip(filething)
+    if name.endswith(".xz"):
+        return Unxz(filething)
+    if name.endswith(".lz4"):
+        return Unlz4(filething)
+    if name.endswith(".bz2"):
+        return Bunzip2(filething)
+
+    return filething
+
+
+EnumerateReads = make_pipe(
+    "EnumerateReads",
+    ['awk',
+     '{ if ((NR-1) % 4 == 0) { print "@"(NR-1)/4 }'
+     'else { print } }']
+)
+LineCount = make_pipe("LineCount", ['wc', '-l'])
+
+
+@timed("Counting reads in input files")
+def fastq_count_reads(filename):
+    with LineCount(decompressed(filename)) as f:
+        return int(f.next().strip()) / 4
+
+
+def filter_fastq(infile, readnames, outfile=None):
+    if outfile is None:
+        outfile = NamedTemporaryFile(suffix="filtered.fq")
+
+    for line in infile:
+        if line[1:].strip() in readnames:
+            outfile.write(line)
+            for n in range(3):
+                outfile.write(infile.next())
+        else:
+            for n in range(3):
+                infile.next()
+
+    outfile.seek(0)
+
+    return outfile
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,10 +1,12 @@
 """Tests functions in Emirge.io"""
 
+import re
+import os
 from StringIO import StringIO
 from subprocess import Popen, PIPE
 from tempfile import NamedTemporaryFile
-
 from Emirge import io
+from nose.tools import assert_true, assert_false, assert_equal, assert_raises
 
 # === FASTA test data ===
 
@@ -34,6 +36,16 @@ sequence_sample = (
 
 read_file_1 = "tests/test_data/ten_seq_community_000_50K_L150_I350.2.fastq"
 
+# === helper functions ===
+
+
+def assert_re_match(regex, string):
+    try:
+        assert re.match(regex, string) is not None
+    except AssertionError as e:
+        e.args += ('"{}" does not match regex "{}"'.format(string, regex),)
+        raise
+
 
 # === test functions ===
 
@@ -46,7 +58,6 @@ def test_Record_empty():
 
 def test_Record_formatting():
     record = io.Record(title="id123", sequence=sequence_sample)
-    print sequence_sample
     assert str(record) == fasta_sample_60
 
 
@@ -56,7 +67,7 @@ def test_FastIterator():
     i = 0
     for record in io.FastIterator(fasta_file):
         assert str(record) == fasta_sample_60
-        i = i+1
+        i += 1
     assert i == n
 
 
@@ -65,39 +76,143 @@ def cmp_reindexed_fq_files(orig, reindexed, nseq):
     lineno = 0
     for sline, dline in zip(orig, reindexed):
         if lineno % 4 == 0:
-            assert dline.rstrip() == "@" + str(lineno/4)
+            assert_equal(dline.rstrip(), "@" + str(lineno/4))
         else:
-            assert dline == sline
+            assert_equal(dline, sline)
         lineno += 1
-    assert nseq == lineno / 4
+    assert_equal(nseq, lineno / 4)
 
 
-def test_ReindexReads():
+def test_reindex_reads():
     nlines = 4 * 1200
 
     # prep input files
     src = NamedTemporaryFile()
-    src_zipped = NamedTemporaryFile(suffix=".gz")
-    zipper = Popen(["gzip", "-c"], stdin=PIPE, stdout=src_zipped).stdin
-
     with open(read_file_1) as f:
         for line, n in zip(f, range(0, nlines)):
             src.write(line)
-            zipper.write(line)
 
     src.flush()
-    zipper.close()
-    src_zipped.flush()
 
-    dst, n_reads = io.ReindexReads(src.name)
-    assert n_reads == nlines / 4
+    dst, n_reads = io.reindex_reads(src.name)
+    assert_equal(n_reads, nlines / 4)
     cmp_reindexed_fq_files(src, dst, n_reads)
 
-    dst, n_reads = io.ReindexReads(src_zipped.name)
-    assert n_reads == nlines / 4
-    cmp_reindexed_fq_files(src, dst, n_reads)
+
+def test_reindex_reads_zipped():
+    nlines = 4 * 1200
+    # prep input files
+    src = NamedTemporaryFile(suffix=".gz")
+    zipper = Popen(["gzip", "-c"], stdin=PIPE, stdout=src)
+
+    with open(read_file_1) as f:
+        for line, n in zip(f, range(0, nlines)):
+            zipper.stdin.write(line)
+
+    zipper.stdin.close()
+    zipper.wait()
+    src.flush()
+
+    dst, n_reads = io.reindex_reads(src.name)
+
+    assert_equal(n_reads, nlines / 4)
+    with open(read_file_1) as f:
+        cmp_reindexed_fq_files(f, dst, n_reads)
+
+
+def test_NamedPipe():
+    pipe = io.NamedPipe()
+    pipe_file = pipe.name
+    assert_true(io.ispipe(pipe_file))
+    del pipe
+    assert_false(io.ispipe(pipe_file))
+
+
+def test_InputFileName():
+    io.InputFileName(read_file_1, check=True)
+    with assert_raises(Exception) as ex:
+        io.InputFileName("/tmp", check=True)
+    assert_re_match(".*is a dir.*", ex.exception.args[0])
+    with assert_raises(Exception) as ex:
+        io.InputFileName("/tmp/this_should_not_exist_d9js9d$HHx", check=True)
+    assert_re_match(".*does not exist.*", ex.exception.args[0])
+    tmpfile = NamedTemporaryFile()
+    os.chmod(tmpfile.name, 0)
+    with assert_raises(Exception) as ex:
+        io.InputFileName(tmpfile.name, check=True)
+    assert_re_match(".*cannot be read.*", ex.exception.args[0])
+
+
+def test_OutputFileName():
+    io.OutputFileName("/tmp/valid_output_file_lkjad9k", check=True)
+    with assert_raises(Exception) as ex:
+        io.OutputFileName("/tmp")
+    assert_re_match(".*is a dir.*", ex.exception.args[0])
+    with assert_raises(Exception) as ex:
+        io.OutputFileName("/sbin/cannot_write_here")
+    assert_re_match(".*directory is not writable.*", ex.exception.args[0])
+    with assert_raises(Exception) as ex:
+        io.OutputFileName("/this_path_no_exists/filename")
+    assert_re_match(".*directory does not exist.*", ex.exception.args[0])
+    tmpfile = NamedTemporaryFile()
+    with assert_raises(Exception) as ex:
+        io.OutputFileName(tmpfile.name, overwrite=False)
+    assert_re_match(".*cowardly refusing to overwrite.*", ex.exception.args[0])
+    os.chmod(tmpfile.name, 0)
+    with assert_raises(Exception) as ex:
+        io.OutputFileName(tmpfile.name, overwrite=True)
+    assert_re_match(".*write protected.*", ex.exception.args[0])
+
+
+def test_decompressed():
+    data = ["this is a test\n", "and a second line\n"]
+    methods = [("gzip", "gz"), ("lz4", "lz4"),
+               ("xz", "xz"), ("bzip2", "bz2")]
+
+    for compressor, suffix in methods:
+        src = NamedTemporaryFile(suffix="."+suffix)
+        zipper = io.Popen([compressor, "-c"], stdin=PIPE, stdout=src).stdin
+        zipper.writelines(data)
+        zipper.close()
+        src.flush()
+
+        with io.decompressed(src.name).reader() as f:
+            for orig, test in zip(data, f):
+                assert_equal(orig, test)
+
+
+def test_EnumerateReads():
+    ereads = io.EnumerateReads(io.File(read_file_1))
+    i = 0
+    with ereads as reads:
+        for line in reads:
+            i += 1
+    with ereads as reads:
+        for line in reads:
+            i += 1
+    with ereads as reads, open(read_file_1) as orig_reads:
+        cmp_reindexed_fq_files(orig_reads, reads, 50000)
 
 
 def test_FastqCountReads():
-    n_reads = io.FastqCountReads(read_file_1)
-    assert n_reads == 50000
+    n_reads = io.fastq_count_reads(read_file_1)
+    assert_equal(n_reads, 50000)
+
+
+def test_Pipe_chained():
+    data = io.File(read_file_1)
+    with io.Gunzip(io.Gzip(data)) as f, open(read_file_1) as g:
+        for fline, gline in zip(f, g):
+            assert_equal(fline, gline)
+
+
+def test_Pipe_cmdsubst():
+    cmd = ["cat", io.EnumerateReads(io.File(read_file_1)),
+           io.EnumerateReads(io.File(read_file_1))]
+    pipe = io.make_pipe("test", cmd)
+    i = 0
+    with pipe(None) as f:
+        for line in f:
+            i += 1
+
+    assert_equal(i, 400000)
